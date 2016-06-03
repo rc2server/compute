12366fdff4a5d9fb6fec05a2de2ffeb79a008465
Mark Lilback <mark@lilback.com>
2016-06-03 07:54:57 -0700

    added a class version that zeros all members at init

--- common/ZeroInitializedStruct.hpp
+++ common/ZeroInitializedStruct.hpp
@@ -8,3 +8,14 @@ struct ZeroInitializedStruct {
 	void *operator new(size_t size) { return alloc_zero(size); }
 	void *operator new[](size_t size) { return alloc_zero(size); }
 };
+
+class ZeroInitializedClass {
+public:
+	static void *alloc_zero(size_t size) {
+		void *mem = ::operator new(size);
+		std::memset(mem, 0, size);
+		return mem;
+	}
+	void *operator new(size_t size) { return alloc_zero(size); }
+	void *operator new[](size_t size) { return alloc_zero(size); }
+};
