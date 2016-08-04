b62488568375775808c8ffff3e322f0478fd23f1
Mark Lilback <mark@lilback.com>
2016-08-04 11:44:16 -0400

    adjusted travis to work without RInside in repo

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -4,7 +4,14 @@
 	echo "checking for RInside"
 	if [ ! -f /usr/local/lib/R/site-library/RInside/libs/RInside.h ]; then
 		echo "installing RInside"
-		(cd vendor/RInside; sudo R CMD INSTALL .)
+		wget https://cran.r-project.org/src/contrib/RInside_0.2.13.tar.gz
+		tar zxf RInside_0.2.13.tar.gz
+		cd RInside
+		patch -p1 < vendor/rinside.patch
+		R CMD INSTALL .
+		cd ..
+		rm -rf RInside_0.2.13.tar.gz RInside
+#		(cd vendor/RInside; sudo R CMD INSTALL .)
 
 	fi
 
