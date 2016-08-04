b9f487aae175ece0ecd723d46c5c89ff23e9caf3
Mark Lilback <mark@lilback.com>
2016-08-04 13:29:40 -0400

    setup the database before build

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -2,6 +2,11 @@
 set -v
 
 #if [ $TRAVIS_OS_NAME eq linux ]; then
+	SRCDIR=$PWD
+	echo "cloning root"
+	cd $SRCDIR
+	git clone https://github.com/wvuRc2/rc2.git root
+
 	echo "checking for RInside"
 	if [ ! -f /usr/local/lib/R/site-library/RInside/libs/RInside.h ]; then
 		echo "installing RInside"
@@ -10,13 +15,18 @@ set -v
 		wget https://cran.r-project.org/src/contrib/RInside_0.2.13.tar.gz
 		tar zxf RInside_0.2.13.tar.gz
 		cd RInside
-		patch -p1 < ../vendor/rinside.patch
+		patch -p1 < $SRCDIR/root/containers/dev/rinside.patch
 		R CMD INSTALL .
-		cd ..
+		cd $SRCDIR
 		rm -rf RInside_0.2.13.tar.gz RInside
 #		(cd vendor/RInside; sudo R CMD INSTALL .)
 
 	fi
 
+	echo "configuring database"
+	cd $SRCDIR
+	psql -U rc2 rc2unittest < $SRCDIR/root/rc2.sql
+	psql -U rc2 rc2unittest < $SRCDIR/root/testData.sql
+	
 #fi
 
