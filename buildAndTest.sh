e7016dcb11e1d91929a8fcbb44fb6f612133db6a
Mark Lilback <mark@lilback.com>
2016-08-09 10:56:57 -0400

    removed travis stuff. jenkins script checks exit status

--- buildAndTest.sh
+++ buildAndTest.sh
@@ -3,6 +3,10 @@
 mkdir build
 cd build
 cmake ../compute
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 make
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 make package
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 make test
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
