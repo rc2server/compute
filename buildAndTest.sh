9005c5fdeb9ef6c4ab84a12cd112d175c7ec9651
Mark Lilback <mark@lilback.com>
2016-08-09 11:10:02 -0400

    shell script now sets up database

--- buildAndTest.sh
+++ buildAndTest.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+psql -U postgres -c 'create user rc2'
+psql -U postgres -c 'create database rc2unittest owner rc2'
+psql -U postgres -U rc2 rcunittest -c 'CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public'
+psql -U rc2 rc2unittest < compute/root/rc2.sql
+psql -U rc2 rc2unittest < compute/root/testData.sql
+echo "select 1" | psql -U rc2 rc2unittest
+rc=$?; if [ $rc != 0 ]; then exit $rc; fi
+
 mkdir build
 cd build
 cmake ../compute
