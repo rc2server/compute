b521733d682c3b3aac015218d9db954d6472b57c
Mark Lilback <mark@lilback.com>
2019-09-19 14:36:07 -0400

    removed debug printing. adjust json used in tests

--- tests/variables-t.cpp
+++ tests/variables-t.cpp
@@ -26,7 +26,6 @@ namespace testing {
 		session->execScript("testVar<-22");
 		session->emptyMessages();
 		string msg = "{\"msg\":\"getVariable\", \"argument\":\"testVar\"}";
-		cerr << "HERE sending: " << msg << endl;
 		session->doJson(msg);
 		ASSERT_EQ(session->_messages.size(), 1);
 		json results = session->popMessage();
@@ -36,12 +35,11 @@ namespace testing {
 
 	TEST_F(VarTest, listVariables)
 	{
-		session->doJson("{\"msg\":\"execScript\", \"argument\":\"rm(list=ls())\")");
+		session->doJson("{\"msg\":\"execScript\", \"argument\":\"rm(list=ls())\"}");
 		session->doJson("{\"msg\":\"execScript\", \"argument\":\"z <- c('foo','bar')\"}");
 		session->doJson("{\"msg\":\"execScript\", \"argument\":\"x<-22;y<-11\"}");
 		session->emptyMessages();
 		session->doJson("{\"msg\":\"listVariables\", \"argument\":\"\", \"watch\":true}");
-		cerr << "message size:" << session->_messages.size() << endl;
 		ASSERT_EQ(session->_messages.size(), 1);
 		json results = session->popMessage();
 		ASSERT_TRUE(results["msg"] == "variableupdate");
@@ -60,7 +58,6 @@ namespace testing {
 		session->doJson("{\"msg\":\"execScript\", \"argument\":\"x<-22;y<-11\"}");
 		session->emptyMessages();
 		session->doJson("{\"msg\":\"listVariables\", \"argument\":\"\", \"watch\":true}");
-		cerr << "message size:" << session->_messages.size() << endl;
 		ASSERT_EQ(session->_messages.size(), 1);
 		json results = session->popMessage();
 		ASSERT_TRUE(results["msg"] == "variableupdate");
@@ -154,20 +151,24 @@ namespace testing {
 		ASSERT_EQ(df["columns"][1]["values"][1], 117.0);
 	}
 
+	// this test fails when run after other tests because of something to do with the environment. It returns two execComplete messages when second should be ShowOutput
 	TEST_F(VarTest, complexDframe) {
 		session->emptyMessages();
+		session->clearGlobalEnvironment();
 		EnvironmentWatcher watcher(Rcpp::Environment::global_env(), session->getExecCallback());
 		// CRASH: using tmp dir before created in session open
 		session->copyFileToWorkingDirectory("dframe.R");
 		fileManager->addFile(4, "dframe.R", 1);
-		session->executeDelayedJson("{\"msg\":\"execFile\", \"argument\":\"4\"}");
+		session->executeDelayedJson("{\"msg\":\"execFile\", \"argument\":\"4\", \"watchVariables\": true}");
 		session->startCountdown(2);
 		session->startEventLoop();
 		ASSERT_EQ(session->_messages.size(), 2);
-		json results = session->popMessage();
+		json results = session->popMessage(); 
+		if (results["msg"] == "execComplete") {
+			results = session->popMessage();
+		}
 		ASSERT_TRUE(results["msg"] == "results");
 		json df = watcher.toJson("cdf");
-//		cout << "val = " << df.dump(4) << endl;
 		int nrow = df["nrow"];
 		int ncol = df["ncol"];
 		ASSERT_EQ(ncol, 4);
@@ -203,7 +204,6 @@ namespace testing {
 		EnvironmentWatcher watcher(Rcpp::Environment::global_env(), session->getExecCallback());
 		session->execScript("mat <- matrix(data=c(2.1, NaN, 4.3, -Inf), nrow=2, ncol=2, dimnames=list(c(\\\"x\\\",\\\"y\\\"), c(\\\"foo\\\",\\\"bar\\\")))");
 		json mat = watcher.toJson("mat");
-		cerr << "val = " << mat.dump(4) << endl;
 		int numrows = mat["nrow"];
 		ASSERT_EQ(numrows, 2);
 		int numcols = mat["ncol"];
@@ -218,7 +218,6 @@ namespace testing {
 		session->execScript("x <- y / 4");
 		json delta = {};
 		watcher.addVariables(delta, true, 1);
-		cout << delta.dump() << endl;
 		ASSERT_EQ(delta["variablesAdded"].size(), 1);
 		ASSERT_EQ(delta["variablesRemoved"].size(), 0);
 		ASSERT_EQ(delta["variablesAdded"]["x"]["type"], "d");
@@ -239,7 +238,6 @@ namespace testing {
 		session->execScript("x <- y / 4");
 		json delta = R"({"foo": "bar"})"_json;;
 		watcher.addVariables(delta, true, 1);
-		cout << delta << endl;
 		ASSERT_EQ(delta["variablesAdded"].size(), 1);
 		ASSERT_EQ(delta["variablesRemoved"].size(), 0);
 		ASSERT_EQ(delta["variablesAdded"]["x"]["type"], "d");
