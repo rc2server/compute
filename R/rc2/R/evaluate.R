27311df6d1e60d2c1f01649ab357c9ea4bafa8a4
Mark Lilback <mark@lilback.com>
2020-06-24 14:22:02 -0400

    updated package so evaluate functionality is usable

--- R/rc2/R/evaluate.R
+++ R/rc2/R/evaluate.R
@@ -1,22 +1,20 @@
 
 evaluateWrapper <- function(parent = emptyenv()) {
-  myEnv <- new.env(parent = parent)
+ myEnv <- new.env(parent = parent)
   assign("allItems", list(), envir = myEnv)
-  
+
   convertItem <- function(item) {
     switch(class(item),
            "rc2src" = list(type = "source", src = item$data$src),
            "rc2plot" = list(type = "plot", filename = attr(item, "filename")),
            "rc2msg" = list(type = "message", message = item$data$message),
            "rc2err" = list(type = "error", message = item$data$message, call = item$data$call),
-           
-      
     )
   }
-  
+
   rc2evaluate <- function(src, baseImageName = "previewImage") {
     assign("allItems", list(), envir = myEnv)
-    evaluate(src, output_handler = outputHandler, stop_on_error = 1L)
+    eres <- evaluate(src, output_handler = outputHandler, stop_on_error = 1L)
     all <- get("allItems", envir = myEnv)
     all2 <- processItems(all, baseImageName)
     return(all2)
@@ -30,17 +28,16 @@ evaluateWrapper <- function(parent = emptyenv()) {
       all[[length(all) + 1]] = newval
       assign("allItems", all, envir = myEnv)
       invisible(NULL)
-    }, 
+    },
     text=function(txt) {
-      #    writeLines("text", con = tc)
-    }, 
+    },
     message=function(msg) {
       all = get("allItems", envir = myEnv)
       newval <- list(data=msg)
       class(newval) <- "rc2msg"
       all[[length(all) + 1]] = newval
       assign("allItems", all, envir = myEnv)
-      
+
       invisible(NULL)
     },
     warning=function(msg) {
@@ -52,9 +49,9 @@ evaluateWrapper <- function(parent = emptyenv()) {
       class(newval) <- "rc2err"
       all[[length(all) + 1]] = newval
       assign("allItems", all, envir = myEnv)
-      
+
       invisible(NULL)
-    }, 
+    },
     graphics = function(data) {
       all = get("allItems", envir = myEnv)
       newval <- structure(list(img = data), class = "rc2plot")
@@ -73,7 +70,7 @@ evaluateWrapper <- function(parent = emptyenv()) {
       class(newval) <- "rc2value"
       all[[length(all) + 1]] = newval
       assign("allItems", all, envir = myEnv)
-      
+
       invisible(NULL)
     })
   saveImage <- function(img, name, ...) {
@@ -81,5 +78,5 @@ evaluateWrapper <- function(parent = emptyenv()) {
     replayPlot(img)
     dev.off()
   }
-  structure(list(evaluate=rc2evaluate, saveImage=saveImage, evaluateWrapper=evaluateWrapper))
+  structure(list(evaluate=rc2evaluate, saveImage=saveImage))
 }
