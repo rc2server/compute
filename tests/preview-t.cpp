d0a14a60bc7949c33d25d338b2f35362c9ee4716
Mark Lilback <mark@lilback.com>
2020-06-26 19:47:17 -0400

    added preview tests

---
+++ tests/preview-t.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <memory>
+using std::cout;
+using std::endl;
+using std::string;
+
+namespace RC2 {
+	namespace testing {
+		TEST(PreviewTest, basic)
+		{
+			ASSERT_TRUE(true);
+		}
+		
+		
+	}; // end testing namespace
+}; // end RC2 namespace
