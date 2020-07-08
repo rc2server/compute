f02319908a795ec562f735b4d2cb4c05f79f4b37
Mark Lilback <mark@lilback.com>
2020-07-08 16:47:48 -0400

    moved error codes to separate file

---
+++ src/Errors.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#define kError_Unknown -1
+#define kError_Open_InvalidDir 101
+#define kError_Open_CreateDirFailed 102
+#define kError_Execfile_InvalidInput 103
+#define kError_ExecFile_MarkdownFailed 104
+#define kError_UnknownFile 105
+#define kError_MissingTransactionId 106
+#define kError_MissingQueryId 107
+#define kError_SweaveError 108
+#define kError_SweaveErrorInLogFile 109
+#define kError_InvalidArgument 110
+#define kError_AlreadyExists 111
+#define kError_QueryFailed 201
+
--- src/RSession.hpp
+++ src/RSession.hpp
@@ -7,25 +7,13 @@
 #include <boost/noncopyable.hpp>
 #include "JsonCommand.hpp"
 #include "SessionCommon.hpp"
+#include "Errors.hpp"
 
 using std::string;
 
 extern const uint32_t kRSessionMagicNumber;
 
 class RInside;
-
-#define kError_Unknown -1
-#define kError_Open_InvalidDir 101
-#define kError_Open_CreateDirFailed 102
-#define kError_Execfile_InvalidInput 103
-#define kError_ExecFile_MarkdownFailed 104
-#define kError_UnknownFile 105
-#define kError_MissingTransactionId 106
-#define kError_MissingQueryId 107
-#define kError_SweaveError 108
-#define kError_SweaveErrorInLogFile 109
-#define kError_InvalidArgument 110
-#define kError_AlreadyExists 111
 
 typedef std::function<void(string&)> OutputCallback;
 //typedef void (^OutputCallback)(string &str);
