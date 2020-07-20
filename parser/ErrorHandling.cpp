d4af89006ed0fbf5863cb6abfbcf013d5a9f73e9
Mark Lilback <mark@lilback.com>
2020-07-20 13:15:36 -0400

    parser has own exception, error codes

--- parser/ErrorHandling.cpp
+++ parser/ErrorHandling.cpp
@@ -2,5 +2,5 @@
 
 void LexerErrorHandler::syntaxError ( antlr4::Recognizer* recognizer, antlr4::Token* offendingSymbol, size_t line, size_t charPositionInLine, const std::string& msg, std::exception_ptr e ) 
 {
-	throw new std::exception();
+	throw ParserException(kParserError_SyntaxError);
 }
--- parser/ErrorHandling.hpp
+++ parser/ErrorHandling.hpp
@@ -6,3 +6,16 @@ class LexerErrorHandler: public antlr4::BaseErrorListener {
 public:
 		void syntaxError ( antlr4::Recognizer * recognizer, antlr4::Token * offendingSymbol, size_t line, size_t charPositionInLine, const std::string & msg, std::exception_ptr e ) override;
 };
+
+class ParserException: public std::exception {
+public:
+	ParserException(int eCode)
+		: code(eCode)
+		{}
+		
+	int getCode() const { return code; }
+protected:
+	int code;
+};
+
+#define kParserError_SyntaxError 701
