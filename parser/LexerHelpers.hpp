932db211a8fc3239a43285a4b7756635d543686b
Chris Grant <chrisgrant.datascience@gmail.com>
2021-03-27 20:24:16 -0400

    Added includes to see if necessary headers were preventing LexerHelpers from working as expected.

--- parser/LexerHelpers.hpp
+++ parser/LexerHelpers.hpp
@@ -1,3 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <common/ZeroInitializedStruct.hpp>
+#include "antlr4-common.h"
+#include "antlr4-runtime.h"
+
+using namespace antlr4;
+
+
     bool isEQStart();
     bool isInlineEqStart();
 	bool isCodeStartBackticks();
